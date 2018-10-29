770ae6f7eae484ba1182d8a449f7f8bba1b72a52
Aikdanai Sidhikosol <aikdanai.s@cleverse.com>
2018-10-29 22:16:03 +0700

    why need lunr when you have vanilla js :tada:

--- source/javascripts/app/_search.js
+++ source/javascripts/app/_search.js
@@ -1,98 +1,84 @@
-//= require ../lib/_lunr
 //= require ../lib/_jquery
 //= require ../lib/_jquery.highlight
-;(function () {
-  'use strict';
 
-  var content, searchResults;
-  var highlightOpts = { element: 'span', className: 'search-highlight' };
-  var searchDelay = 0;
-  var timeoutHandle = 0;
+;(function() {
+  'use strict'
 
-  var index = new lunr.Index();
+  var content, searchResults
+  var highlightOpts = { element: 'span', className: 'search-highlight' }
+  var searchDelay = 300
+  var timeoutHandle = 0
+  const dicts = []
 
-  index.ref('id');
-  index.field('title', { boost: 10 });
-  index.field('body');
-  index.pipeline.add(lunr.trimmer, lunr.stopWordFilter);
-
-  $(populate);
-  $(bind);
+  $(populate)
+  $(bind)
 
   function populate() {
     $('h1, h2').each(function() {
-      var title = $(this);
-      var body = title.nextUntil('h1, h2');
-      index.add({
+      var title = $(this)
+      var body = title.nextUntil('h1, h2')
+      const obj = {
         id: title.prop('id'),
         title: title.text(),
         body: body.text()
-      });
-    });
-
-    determineSearchDelay();
-  }
-  function determineSearchDelay() {
-    if(index.tokenStore.length>5000) {
-      searchDelay = 300;
-    }
+      }
+      dicts.push(obj)
+    })
   }
 
   function bind() {
-    content = $('.content');
-    searchResults = $('.search-results');
-
-    $('#input-search').on('keyup',function(e) {
-      var wait = function() {
-        return function(executingFunction, waitTime){
-          clearTimeout(timeoutHandle);
-          timeoutHandle = setTimeout(executingFunction, waitTime);
-        };
-      }();
-      wait(function(){
-        search(e);
-      }, searchDelay );
-    });
+    content = $('.content')
+    searchResults = $('.search-results')
+    $('#input-search').on('keyup', function(e) {
+      var wait = (function() {
+        return function(executingFunction, waitTime) {
+          clearTimeout(timeoutHandle)
+          timeoutHandle = setTimeout(executingFunction, waitTime)
+        }
+      })()
+      wait(function() {
+        search(e)
+      }, searchDelay)
+    })
   }
 
   function search(event) {
-
-    var searchInput = $('#input-search')[0];
-
-    unhighlight();
-    searchResults.addClass('visible');
-
+    var searchInput = $('#input-search')[0]
+    unhighlight()
+    searchResults.addClass('visible')
     // ESC clears the field
-    if (event.keyCode === 27) searchInput.value = '';
-
+    if (event.keyCode === 27) searchInput.value = ''
     if (searchInput.value) {
-      var results = index.search(searchInput.value).filter(function(r) {
-        return r.score > 0.0001;
-      });
-
+      var results = dicts.filter(function(dict) {
+        const val = searchInput.value
+        return dict.title.includes(val) || dict.body.includes(val)
+      })
       if (results.length) {
-        searchResults.empty();
-        $.each(results, function (index, result) {
-          var elem = document.getElementById(result.ref);
-          searchResults.append("<li><a href='#" + result.ref + "'>" + $(elem).text() + "</a></li>");
-        });
-        highlight.call(searchInput);
+        searchResults.empty()
+        $.each(results, function(index, result) {
+          var elem = document.getElementById(result.id)
+          searchResults.append(
+            "<li><a href='#" + result.id + "'>" + $(elem).text() + '</a></li>'
+          )
+        })
+        highlight.call(searchInput)
       } else {
-        searchResults.html('<li></li>');
-        $('.search-results li').text('No Results Found for "' + searchInput.value + '"');
+        searchResults.html('<li></li>')
+        $('.search-results li').text(
+          'No Results Found for "' + searchInput.value + '"'
+        )
       }
     } else {
-      unhighlight();
-      searchResults.removeClass('visible');
+      unhighlight()
+      searchResults.removeClass('visible')
     }
   }
 
   function highlight() {
-    if (this.value) content.highlight(this.value, highlightOpts);
+    if (this.value) content.highlight(this.value, highlightOpts)
   }
 
   function unhighlight() {
-    content.unhighlight(highlightOpts);
+    content.unhighlight(highlightOpts)
   }
-})();
-
+})()
