9b3e884c0a0d5d80f64da2477cc1d6d022d4dacc
Aikdanai Sidhikosol <aikdanai.s@cleverse.com>
2018-10-30 14:38:52 +0700

    add clear button for search box

--- source/javascripts/app/_search.js
+++ source/javascripts/app/_search.js
@@ -4,7 +4,7 @@
 ;(function() {
   'use strict'
 
-  var content, searchResults
+  var content, searchResults, searchClear
   var highlightOpts = { element: 'span', className: 'search-highlight' }
   var searchDelay = 300
   var timeoutHandle = 0
@@ -29,6 +29,8 @@
   function bind() {
     content = $('.content')
     searchResults = $('.search-results')
+    searchClear = $('#search-clear')
+    searchClear.hide()
     $('#input-search').on('keyup', function(e) {
       var wait = (function() {
         return function(executingFunction, waitTime) {
@@ -40,6 +42,12 @@
         search(e)
       }, searchDelay)
     })
+    searchClear.on('click', function(e) {
+      search({
+        ...e,
+        keyCode: 27
+      })
+    })
   }
 
   function search(event) {
@@ -48,7 +56,8 @@
     searchResults.addClass('visible')
     // ESC clears the field
     if (event.keyCode === 27) searchInput.value = ''
-    if (searchInput.value) {
+    if (searchInput.value && searchInput.value.length > 0) {
+      searchClear.show()
       var results = dicts.filter(function(dict) {
         const val = searchInput.value
         return dict.title.includes(val) || dict.body.includes(val)
@@ -70,6 +79,7 @@
       }
     } else {
       unhighlight()
+      searchClear.hide()
       searchResults.removeClass('visible')
     }
   }
